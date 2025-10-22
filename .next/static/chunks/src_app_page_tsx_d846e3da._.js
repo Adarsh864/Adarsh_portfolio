e7d8130b04e0759d524acb342ac11474c1c47da1
adarsh shetty <shettyadarsh321@gmail.com>
2025-10-22 04:59:52 +0000

    Cleanup: Removed unused firebase and genkit packages/files

--- .next/static/chunks/src_app_page_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_e3dbab8f._.js",
-  "static/chunks/src_d039a98c._.js"
-],
-    source: "dynamic"
-});
